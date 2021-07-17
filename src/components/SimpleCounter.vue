1985ac02fe8010557a936a33712991c87f283d27
franciscomarin98 <francmarinc@gmail.com>
2021-07-17 11:17:46 -0500

    feat: Conociendo mas del metodo setup

---
+++ src/components/SimpleCounter.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Counter</h1>
+  <h3>{{ counterComputed }}</h3>
+  <button @click="increment">Aumentar</button>
+  <button @click="decrement">Decrementar</button>
+</template>
+
+<script>
+import {ref, computed} from "vue";
+
+export default {
+  name: "SimpleCounter",
+  setup() {
+    const counter = ref(0);
+    const counterComputed = computed(() => {
+      return `El valor del contador es: ${counter.value}`;
+    });
+    const increment = () => {
+      counter.value++;
+    }
+    const decrement = () => {
+      counter.value--;
+    }
+
+    return {counterComputed, increment, decrement}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
+  <SimpleCounter />
+  <hr>
+  <hr>
   <HelloWorld/>
 </template>
 
 <script>
 import HelloWorld from "../components/HelloWorld";
+import SimpleCounter from "../components/SimpleCounter";
 
 export default {
   name: 'Home',
   components: {
+    SimpleCounter,
     HelloWorld
   }
 }
