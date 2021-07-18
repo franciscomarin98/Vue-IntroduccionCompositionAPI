2067c2f3c9c31af3ec4ce7c35b361e6d3fd42949
franciscomarin98 <francmarinc@gmail.com>
2021-07-18 10:47:17 -0500

    Aprendiendo sobre la funcion watch

---
+++ src/components/WatchCounter.vue
@@ -0,0 +1,58 @@
+<template>
+  <h1>Watch Counter</h1>
+  <h3>{{ counter }}</h3>
+  <h3>{{ counter2 }}</h3>
+  <p>{{ message }}</p>
+  <button @click="increment">Aumentar</button>
+  <button @click="decrement">Decrementar</button>
+</template>
+
+<script>
+import {ref, watch} from 'vue';
+
+export default {
+  name: "WatchCounter",
+  setup() {
+    const counter = ref(0);
+    const counter2 = ref(0);
+    const message = ref('');
+
+    // watch(counter, ()=>{
+    //   if(counter.value === 5){
+    //     message.value = "El valor de counter es 5";
+    //   }else{
+    //     message.value = ""
+    //   }
+    // });
+
+    watch([counter], (currentValue, oldValue) => {
+      console.log('Current value: ', currentValue);
+      console.log('Old value: ', oldValue);
+
+      if (counter.value === 5) {
+        message.value = "El valor de counter es 5";
+        counter2.value = 10;
+      } else {
+        message.value = ""
+      }
+
+      if (counter2.value === 10) {
+        console.log('El counter 2 ha cambiado');
+      }
+
+    })
+
+    const increment = () => {
+      counter.value++;
+    }
+    const decrement = () => {
+      counter.value--;
+    }
+    return {counter, counter2, message, increment, decrement}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,6 @@
 <template>
+  <WatchCounter/>
+  <hr/>
   <ReactiveCounter/>
   <hr/>
   <SimpleCounter/>
@@ -10,10 +12,12 @@
 import HelloWorld from "../components/HelloWorld";
 import SimpleCounter from "../components/SimpleCounter";
 import ReactiveCounter from "../components/ReactiveCounter";
+import WatchCounter from "../components/WatchCounter";
 
 export default {
   name: 'Home',
   components: {
+    WatchCounter,
     ReactiveCounter,
     SimpleCounter,
     HelloWorld
